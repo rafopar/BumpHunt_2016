0e9a3fd80613bed8ddd2ed28137c41d5e27ec501
Rafayel Paremuzyan <rafopar@jlab.org>
2019-10-03 17:13:58 -0400

    For the Bottom clusters, the time cut function a little bit released. Only the 1st parameter is modified

--- pass4/codes/InitSettings.C
+++ pass4/codes/InitSettings.C
@@ -4,9 +4,9 @@
 void InitVariables() {
 
     f_clTBotUpLim = new TF1("f_clTBotUpLim", "[0] + x*( [1] + x*[2] )", 0., 2.5);
-    f_clTBotUpLim->SetParameters(57.6406, 3.40282, -1.00306);
+    f_clTBotUpLim->SetParameters(58.5, 3.40282, -1.00306);
     f_clTBotLowLim = new TF1("f_clTBotLowLim", "[0] + x*( [1] + x*[2] )", 0., 2.5);
-    f_clTBotLowLim->SetParameters(47.1781, 7.55268, -1.89745);
+    f_clTBotLowLim->SetParameters(45.51, 7.55268, -1.89745);
     f_clTTopUpLim = new TF1("f_clTTopUpLim", "[0] + x*( [1] + x*[2] )", 0., 2.5);
     f_clTTopUpLim->SetParameters(58.4842, 6.33371, -3.54136);
     f_clTTopLowLim = new TF1("f_clTTopLowLim", "[0] + x*( [1] + x*[2] )", 0., 2.5);
