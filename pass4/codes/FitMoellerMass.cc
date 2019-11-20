a31d5494200fef3459b5d5cf6336b11fd3b46351
Rafayel Paremuzyan <rafopar@jlab.org>
2019-11-20 10:27:52 -0500

    The FitMoellerMass.cc now in addition to sigma ws chi2Cut, also plots the Sqrt(N) and the Fig of Merit as a function of Chi2Cut

--- pass4/codes/FitMoellerMass.cc
+++ pass4/codes/FitMoellerMass.cc
@@ -54,7 +54,7 @@ int main(int argc, char** argv) {
 
     TCanvas *c1 = new TCanvas("c1", "", 950, 950);
     c1->SetLeftMargin(0.15);
-    
+
     TF1 *f_Gaus = new TF1("f_Gaus", "[0]*TMath::Gaus(x, [1], [2])", 0., 0.07);
     f_Gaus->SetNpx(4500);
     f_Gaus->SetParLimits(2, 0., 0.02);
@@ -63,40 +63,80 @@ int main(int argc, char** argv) {
 
     TH2D *h_chi2Minv = (TH2D*) file_in->Get("h_Chi2NDF_TCM4");
 
-    TGraph *gr1 = new TGraph();
-    gr1->SetMarkerStyle(20);
-    gr1->SetMarkerColor(2);
+    h_chi2Minv->SetTitle("; M(e^{-}e^{-}) [GeV]; #chi^{2}/MDF");
+    h_chi2Minv->Draw("colz");
+    c1->Print("Figs/Chi2NDF_Mass_Moeller_Data.eps");
+    c1->Print("Figs/Chi2NDF_Mass_Moeller_Data.pdf");
+    c1->Print("Figs/Chi2NDF_Mass_Moeller_Data.png");
+    
+    TH1D *h_Chi2Proj1 = (TH1D*)h_chi2Minv->ProjectionY("h_Chi2Proj1", 1, h_chi2Minv->GetXaxis()->FindBin(0.055) );
+    
+    TGraph *grSigma = new TGraph();
+    grSigma->SetTitle("; #chi^{2}/NDF cut; #sigma [GeV]");
+    grSigma->SetMarkerStyle(20);
+    grSigma->SetMarkerColor(2);
+
+    TGraph *grNMoeller = new TGraph();
+    grNMoeller->SetTitle("; #chi^{2}/NDF cut; #sqrt{N}");
+    grNMoeller->SetMarkerStyle(20);
+    grNMoeller->SetMarkerColor(2);
+
+    TGraph *grFigOfMerit = new TGraph();
+    grFigOfMerit->SetTitle("; #chi^{2}/NDF cut; #sqrt{N}/#sigma");
+    grFigOfMerit->SetMarkerStyle(20);
+    grFigOfMerit->SetMarkerColor(2);
 
     int ind_chi2 = 0;
 
+    c1->Print("Figs/MoellerMassFits.pdf[");
     for (int i = 0; i < h_chi2Minv->GetNbinsY(); i++) {
 
         TH1D *htmp = (TH1D*) h_chi2Minv->ProjectionX("tmp", 1, i + 1);
         cout << "Kuku" << endl;
 
         double chi2Cut = h_chi2Minv->GetYaxis()->GetBinCenter(i + 1);
+        double NMoeller = h_Chi2Proj1->Integral(1, i+1);
 
         if (htmp->GetEntries() > 250) {
 
-            if( ind_chi2 == 0 ){
+            if (ind_chi2 == 0) {
                 f_Gaus->SetParameters(htmp->GetMaximum(), 0.048, 0.002);
             }
-            
+
             htmp->Fit(f_Gaus, "MeV", "", 0.03, 0.052);
+            c1->Print("Figs/MoellerMassFits.pdf");
 
             double sigm = f_Gaus->GetParameter(2);
 
-            gr1->SetPoint(ind_chi2, chi2Cut, sigm);
+            grSigma->SetPoint(ind_chi2, chi2Cut, sigm);
+            grNMoeller->SetPoint(ind_chi2, chi2Cut, sqrt(NMoeller));
 
+            grFigOfMerit->SetPoint(ind_chi2, chi2Cut, sqrt(NMoeller)/sigm);
+            
             ind_chi2 = ind_chi2 + 1;
         }
 
     }
+    c1->Print("Figs/MoellerMassFits.pdf]");
 
-    gr1->Draw("AP");
-    c1->Print("Figs/MassSigmaChi2Cut.eps");
-    c1->Print("Figs/MassSigmaChi2Cut.pdf");
-    c1->Print("Figs/MassSigmaChi2Cut.png");
+    c1->Clear();
+    grSigma->GetYaxis()->SetTitleOffset(2.);
+    grSigma->Draw("AP");
+    c1->Print("Figs/MassSigmaChi2Cut_Data.eps");
+    c1->Print("Figs/MassSigmaChi2Cut_Data.pdf");
+    c1->Print("Figs/MassSigmaChi2Cut_Data.png");
+    
+    c1->Clear();
+    grNMoeller->Draw("AP");
+    c1->Print("Figs/Sqrt_NMoeller_Data.eps");
+    c1->Print("Figs/Sqrt_NMoeller_Data.pdf");
+    c1->Print("Figs/Sqrt_NMoeller_Data.png");
+    
+    c1->Clear();
+    grFigOfMerit->Draw("AP");
+    c1->Print("Figs/MoellerFigOfMerit_Data.eps");
+    c1->Print("Figs/MoellerFigOfMerit_Data.pdf");
+    c1->Print("Figs/MoellerFigOfMerit_Data.png");
 
     return 0;
 }
---
+++ pass4/codes/compile_FitMoellerMass.sh
@@ -0,0 +1,1 @@
+g++ FitMoellerMass.cc -o FitMoellerMass.exe `root-config --cflags --libs`
