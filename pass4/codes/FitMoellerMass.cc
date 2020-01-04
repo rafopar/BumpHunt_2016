3c451b4597d771a9f2cb60910ba988f9d1e66359
Rafayel Paremuzyan <rafopar@jlab.org>
2020-01-04 14:00:01 -0500

    Is drawing now more histograms, and fiducial regions

--- pass4/codes/FitMoellerMass.cc
+++ pass4/codes/FitMoellerMass.cc
@@ -24,6 +24,9 @@ using namespace std;
 /*
  * 
  */
+
+void DrawMoellerFiducial();
+
 int main(int argc, char** argv) {
 
     gStyle->SetOptFit(1);
@@ -47,7 +50,11 @@ int main(int argc, char** argv) {
 
     if (dataSet.compare("Data") == 0) {
         inpFileName = "MoellerAnalyze_Data_All.root";
+    } else if (dataSet.compare("MC") == 0) {
+        inpFileName = "MoellerAnalyze_MC_All.root";
     } else {
+        cout << "Wrong Argument is provided." << endl;
+        cout << "Exiting" << endl;
         exit(1);
     }
 
@@ -104,23 +111,23 @@ int main(int argc, char** argv) {
     c1->Print(Form("Figs/Trk_dT_Psum5_%s.png", dataSet.c_str()));
 
     TH1D *h_dT_tr5 = (TH1D*) h_Trk_dT_Psum5->ProjectionY("h_dT_tr5", h_Trk_dT_Psum5->GetXaxis()->FindBin(PSumMin), h_Trk_dT_Psum5->GetXaxis()->FindBin(PSumMax));
-    f_Gaus->SetParameters(h_dT_tr5->GetMaximum(), h_dT_tr5->GetMean(), h_dT_tr5->GetRMS() );
+    f_Gaus->SetParameters(h_dT_tr5->GetMaximum(), h_dT_tr5->GetMean(), h_dT_tr5->GetRMS());
     h_dT_tr5->Fit(f_Gaus, "MeV", "", -3., 1.5);
     double mean_trDt = f_Gaus->GetParameter(1);
     double sigm_trDt = f_Gaus->GetParameter(2);
-    line1->DrawLine(mean_trDt - 1.5*sigm_trDt, 0., mean_trDt - 1.5*sigm_trDt, h_dT_tr5->GetMaximum() );
-    line1->DrawLine(mean_trDt + 1.5*sigm_trDt, 0., mean_trDt + 1.5*sigm_trDt, h_dT_tr5->GetMaximum() );
+    line1->DrawLine(mean_trDt - 1.5 * sigm_trDt, 0., mean_trDt - 1.5 * sigm_trDt, h_dT_tr5->GetMaximum());
+    line1->DrawLine(mean_trDt + 1.5 * sigm_trDt, 0., mean_trDt + 1.5 * sigm_trDt, h_dT_tr5->GetMaximum());
     c1->Print(Form("Figs/dT_tr5_%s.eps", dataSet.c_str()));
     c1->Print(Form("Figs/dT_tr5_%s.pdf", dataSet.c_str()));
     c1->Print(Form("Figs/dT_tr5_%s.png", dataSet.c_str()));
 
-    TH2D *h_chi2Minv = (TH2D*) file_in->Get("h_Chi2NDF_TCM4");
+    TH2D *h_chi2Minv = (TH2D*) file_in->Get("h_Chi2NDF_TCM6");
 
     h_chi2Minv->SetTitle("; M(e^{-}e^{-}) [GeV]; #chi^{2}/MDF");
     h_chi2Minv->Draw("colz");
-    c1->Print("Figs/Chi2NDF_Mass_Moeller_Data.eps");
-    c1->Print("Figs/Chi2NDF_Mass_Moeller_Data.pdf");
-    c1->Print("Figs/Chi2NDF_Mass_Moeller_Data.png");
+    c1->Print(Form("Figs/Chi2NDF_Mass_Moeller_%s.eps", dataSet.c_str()));
+    c1->Print(Form("Figs/Chi2NDF_Mass_Moeller_%s.pdf", dataSet.c_str()));
+    c1->Print(Form("Figs/Chi2NDF_Mass_Moeller_%s.png", dataSet.c_str()));
 
     TH1D *h_Chi2Proj1 = (TH1D*) h_chi2Minv->ProjectionY("h_Chi2Proj1", 1, h_chi2Minv->GetXaxis()->FindBin(0.055));
 
@@ -141,7 +148,7 @@ int main(int argc, char** argv) {
 
     int ind_chi2 = 0;
     f_Gaus->SetParLimits(2, 0., 0.02);
-    c1->Print("Figs/MoellerMassFits.pdf[");
+    c1->Print(Form("Figs/MoellerMassFits_%s.pdf[", dataSet.c_str()));
     for (int i = 0; i < h_chi2Minv->GetNbinsY(); i++) {
 
         TH1D *htmp = (TH1D*) h_chi2Minv->ProjectionX("tmp", 1, i + 1);
@@ -157,7 +164,7 @@ int main(int argc, char** argv) {
             }
 
             htmp->Fit(f_Gaus, "MeV", "", 0.03, 0.052);
-            c1->Print("Figs/MoellerMassFits.pdf");
+            c1->Print(Form("Figs/MoellerMassFits_%s.pdf", dataSet.c_str()));
 
             double sigm = f_Gaus->GetParameter(2);
             double sigmErr = f_Gaus->GetParError(2);
@@ -172,27 +179,87 @@ int main(int argc, char** argv) {
         }
 
     }
-    c1->Print("Figs/MoellerMassFits.pdf]");
+    c1->Print(Form("Figs/MoellerMassFits_%s.pdf]", dataSet.c_str()));
 
     c1->Clear();
     grSigma->GetYaxis()->SetTitleOffset(2.);
     grSigma->Draw("AP");
-    c1->Print("Figs/MassSigmaChi2Cut_Data.eps");
-    c1->Print("Figs/MassSigmaChi2Cut_Data.pdf");
-    c1->Print("Figs/MassSigmaChi2Cut_Data.png");
+    c1->Print(Form("Figs/MassSigmaChi2Cut_%s.eps", dataSet.c_str()));
+    c1->Print(Form("Figs/MassSigmaChi2Cut_%s.pdf", dataSet.c_str()));
+    c1->Print(Form("Figs/MassSigmaChi2Cut_%s.png", dataSet.c_str()));
 
     c1->Clear();
     grNMoeller->Draw("AP");
-    c1->Print("Figs/Sqrt_NMoeller_Data.eps");
-    c1->Print("Figs/Sqrt_NMoeller_Data.pdf");
-    c1->Print("Figs/Sqrt_NMoeller_Data.png");
+    c1->Print(Form("Figs/Sqrt_NMoeller_%s.eps", dataSet.c_str()));
+    c1->Print(Form("Figs/Sqrt_NMoeller_%s.pdf", dataSet.c_str()));
+    c1->Print(Form("Figs/Sqrt_NMoeller_%s.png", dataSet.c_str()));
 
     c1->Clear();
     grFigOfMerit->Draw("AP");
-    c1->Print("Figs/MoellerFigOfMerit_Data.eps");
-    c1->Print("Figs/MoellerFigOfMerit_Data.pdf");
-    c1->Print("Figs/MoellerFigOfMerit_Data.png");
+    c1->Print(Form("Figs/MoellerFigOfMerit_%s.eps", dataSet.c_str()));
+    c1->Print(Form("Figs/MoellerFigOfMerit_%s.pdf", dataSet.c_str()));
+    c1->Print(Form("Figs/MoellerFigOfMerit_%s.png", dataSet.c_str()));
+
+    TH2D *h_tr_yxc_1 = (TH2D*) file_in->Get("h_tr_yxc_3");
+    h_tr_yxc_1->SetTitle("; X_{tr} [mm]; Y_{tr} [mm]");
+    h_tr_yxc_1->SetMarkerColor(4);
+    h_tr_yxc_1->SetMarkerStyle(20);
+    h_tr_yxc_1->SetMarkerSize(0.12);
+    h_tr_yxc_1->Draw("colz");
+    DrawMoellerFiducial();
+    c1->Print(Form("Figs/tr_yxc_1_%s.eps", dataSet.c_str()));
+    c1->Print(Form("Figs/tr_yxc_1_%s.pdf", dataSet.c_str()));
+    c1->Print(Form("Figs/tr_yxc_1_%s.png", dataSet.c_str()));
+
+    TH2D *h_cl_yxc_1 = (TH2D*) file_in->Get("h_cl_yxc_3");
+    h_cl_yxc_1->SetTitle("; X_{cl} [mm]; Y_{cl} [mm]");
+    h_cl_yxc_1->SetMarkerColor(2);
+    h_cl_yxc_1->SetMarkerStyle(20);
+    h_cl_yxc_1->SetMarkerSize(0.12);
+    h_cl_yxc_1->Draw("colz");
+    c1->Print(Form("Figs/cl_yxc_1_%s.eps", dataSet.c_str()));
+    c1->Print(Form("Figs/cl_yxc_1_%s.pdf", dataSet.c_str()));
+    c1->Print(Form("Figs/cl_yxc_1_%s.png", dataSet.c_str()));
+
+    h_cl_yxc_1->Draw("");
+    h_tr_yxc_1->Draw("Same");
+    DrawMoellerFiducial();
+    c1->Print(Form("Figs/ClTrk_yxc1_%s.eps", dataSet.c_str()));
+    c1->Print(Form("Figs/ClTrk_yxc1_%s.pdf", dataSet.c_str()));
+    c1->Print(Form("Figs/ClTrk_yxc1_%s.png", dataSet.c_str()));
 
     return 0;
 }
 
+void DrawMoellerFiducial() {
+
+    TLine *line1 = new TLine();
+    line1->SetLineColor(1);
+    line1->SetLineWidth(2);
+    line1->DrawLine(-125, 26., -125, 40.);
+    line1->DrawLine(-125, 26., -97, 26.);
+    line1->DrawLine(-97, 26., -97, 40.);
+    line1->DrawLine(-97, 40., -125, 40.);
+    
+    line1->DrawLine(-85, 23., -85, 31.);
+    line1->DrawLine(-85, 23., -60, 23.);
+    line1->DrawLine(-60, 23., -60, 31.);
+    line1->DrawLine(-60, 31., -85, 31.);
+    
+    line1->DrawLine(-112, -27., -112, -40.);
+    line1->DrawLine(-112, -27., -95, -27.);
+    line1->DrawLine(-95, -27., -95, -40.);
+    line1->DrawLine(-95, -40., -112, -40.);
+    
+    line1->DrawLine(-95, -33., -95, -39.);
+    line1->DrawLine(-95, -33., -85, -33.);
+    line1->DrawLine(-85, -33., -85, -39.);
+    line1->DrawLine(-85, -39., -95, -39.);
+    
+    line1->DrawLine(-70, -23., -70, -30.);
+    line1->DrawLine(-70, -23., -48, -23.);
+    line1->DrawLine(-48, -23., -48, -30.);
+    line1->DrawLine(-48, -30., -70, -30.);
+    
+    delete line1;
+}
