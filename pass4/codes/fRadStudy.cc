b17d855467dc2d5fcc11cc1331a803ccf6c54e40
Rafayel Paremuzyan <rafopar@jlab.org>
2020-01-13 17:05:53 -0500

    Code for studying the rad fraction

---
+++ pass4/codes/fRadStudy.cc
@@ -0,0 +1,19 @@
+/* 
+ * File:   fRadStudy.cc
+ * Author: rafopar
+ *
+ * Created on January 13, 2020, 5:05 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
